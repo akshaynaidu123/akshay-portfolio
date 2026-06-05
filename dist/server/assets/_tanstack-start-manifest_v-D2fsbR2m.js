bfd048deb563fef191726992a1880f2a44871e95
akshaynaidu123 <akshayenaganti@gmail.com>
2026-06-05 17:20:52 +0530

    Fix deployment configuration

--- dist/server/assets/_tanstack-start-manifest_v-D2fsbR2m.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/AKSHAY/protifilo/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CgElcoU1.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CgElcoU1.js" } }] }, "/": { filePath: "D:/AKSHAY/protifilo/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-BKfxU6Hp.js"] } } });
-export {
-  tsrStartManifest
-};
